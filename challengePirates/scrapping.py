1e1a8a4255409f69d6be108b750f4fa84c43f567
Luís Farias <luisgmfarias@gmail.com>
2020-10-14 03:43:31 -0300

    uuid for each data

--- challengePirates/scrapping.py
+++ challengePirates/scrapping.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 import pandas as pd
 import time
+import uuid
 
 # Setting webdriver headless option
 options = webdriver.ChromeOptions()
@@ -42,7 +43,7 @@ def pagination(table):
     for row in table.find_elements_by_tag_name('tr')[2:]:
         city = row.find_element_by_xpath('./td[1]')
         cep_range = row.find_element_by_xpath('./td[2]')
-        insert_data(city.text, cep_range.text)
+        insert_data(uuid.uuid4().int, city.text, cep_range.text)
 
     try:
         driver.find_element_by_xpath(
@@ -53,14 +54,14 @@ def pagination(table):
     pagination(driver.find_element_by_class_name('tmptabela'))
 
 
-def insert_data(city, cep_range):
+def insert_data(id, city, cep_range):
     global uf_dataframe
     uf_dataframe = uf_dataframe.append(
-        [{'Localidade': city, 'Faixa de CEP': cep_range}], ignore_index=True)
+        [{'id':id, 'Localidade': city, 'Faixa de CEP': cep_range}], ignore_index=True)
     return 0
 
 
 if __name__ == "__main__":
-    uf_dataframe = pd.DataFrame(columns=['Localidade', 'Faixa de CEP'])
-    get_data(UFs[:2])
+    uf_dataframe = pd.DataFrame(columns=['id','Localidade', 'Faixa de CEP']) #initiating pandas dataframe
+    get_data(UFs[5:6]) #scrapping first four UFs in alphabetical order
     print(uf_dataframe)
