43bdf22ceb6e0f2b3d2ebbc3de109c1d93fdac20
Luís Farias <luisgmfarias@gmail.com>
2020-10-14 19:51:54 -0300

    organizing gitignore file and output file removed

--- challengePirates/tests.py
+++ challengePirates/tests.py
@@ -1,5 +1,9 @@
 from scrapping import get_data, driver_init
 from selenium import webdriver
+import pandas as pd
+
+def init_dataframe():
+    uf_dataframe = pd.DataFrame(columns=['id', 'Localidade', 'Faixa de CEP'])
 
 def test_driver():
     driver = webdriver.Chrome(executable_path=r"driver/chromedriver")
